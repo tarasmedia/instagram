3956008b5be43675a08fd2fcf36dc68a311c2f6f
tarasmedia <posh@live.ru>
2022-12-16 14:49:06 +0300

    production v3

---
+++ db/seeders/20221216114300-first_seed.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Posts', [
+      {picture: 'https://images.hdqwalls.com/download/horizon-zero-dawn-nature-mountains-trees-sky-4k-yj-1600x1200.jpg', text: 'cool!'}]
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Posts', null, {});
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
